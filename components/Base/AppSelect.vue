ff39a454283865ae036a9948803ca521217f3d58
Khaled <khaledhossamshehab@gmail.com>
2023-03-23 15:24:18 +0200

    adding inputs components

---
+++ components/Base/AppSelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <label class="typo__label text-start"></label>
+    <multiselect
+      v-model="localValue"
+      :allow-empty="false"
+      :options="options"
+    ></multiselect>
+  </div>
+</template>
+<script>
+import Multiselect from 'vue-multiselect'
+
+export default {
+  components: { Multiselect },
+  props: {
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+    options: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    localValue: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        if (value) {
+          this.$emit('input', value)
+        } else {
+          this.$emit('input', this.options[0])
+        }
+      },
+    },
+  },
+}
+</script>
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
---
+++ components/Base/AppTitle.vue
@@ -0,0 +1,40 @@
+<template>
+  <h3
+    class="text-gray-700 py-1 text-3xl font-bold flex justify-between"
+    :class="{
+      'underline decoration-4 decoration-red-400': underline,
+      'font-normal': thin,
+      'text-2xl': small,
+    }"
+  >
+    {{ text }}
+    <slot></slot>
+  </h3>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    underline: {
+      type: Boolean,
+      default: false,
+    },
+    thin: {
+      type: Boolean,
+      default: false,
+    },
+    small: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  data() {
+    return {}
+  },
+}
+</script>
